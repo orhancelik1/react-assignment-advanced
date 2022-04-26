6cba168957d75d9526aaec184089e35dfd65eb07
orhan <orhankagan33@gmail.com>
2022-04-27 02:58:16 +0300

    designed

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -2,20 +2,22 @@ import React from "react";
 
 const UserCard = ({ user }) => {
   return (
-    <div class="card">
-      <div className="card-img">
-        <img
-          className="image"
-          alt="avatar"
-          src={`https://avatars.dicebear.com/v2/avataaars/${user.username}.svg?options[mood][]=happy`}
-        />
-      </div>
+    <div class="container">
+      <div className="column">
+        <div className="overflow">
+          <img
+            className="image"
+            alt="avatar"
+            src={`https://avatars.dicebear.com/v2/avataaars/${user.username}.svg?options[mood][]=happy`}
+          />
+        </div>
 
-      <div class="card-body">
-        <h1>{user.name}</h1>
-        <p>{user.email}</p>
-        <p>{user.phone}</p>
-        <p>{user.website}</p>
+        <div class="body-info">
+          <h1>{user.name}</h1>
+          <p>{user.email}</p>
+          <p>{user.phone}</p>
+          <p>{user.website}</p>
+        </div>
       </div>
     </div>
   );
