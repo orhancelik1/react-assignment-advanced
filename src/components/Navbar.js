c3530692e8ccae5f510f2edfc7bf25b1e03433ea
orhan <orhankagan33@gmail.com>
2022-04-27 15:20:28 +0300

    basic nav added

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <nav className="nav--">
+        <p className="nav--p">my Navbar here</p>
+      </nav>
+    </div>
+  );
+};
+export default Navbar;
